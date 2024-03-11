06bc946da98d28c6cdc07e118f4eaa7803ed8c07
henry <henry@10.113.242.240>
2024-03-10 18:05:36 -0700

    Lab02 - Step #4

---
+++ Lab02/src/main/Hello.java
@@ -0,0 +1,20 @@
+package main;
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git!");
+		System.out.println("yang - This line was added by Dr. Yang.");
+		// Add a new line with your information
+		System.out.println("302894241 - This line was added by Henry Muentz.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+
+}
+
+}
