f625829ab1a71afcc6896b9c09a1f0c7b35d4e45
sudha <sudha@Tetsutetsu>
2024-03-10 22:48:20 -0700

    Added SacState ID and name to line 9 oh Hello.java

--- Lab02/src/main/Hello.java
+++ Lab02/src/main/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("302894241 - This line was added by Henry Muentz.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("303019197 - This line was added by Sudha Dasu.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
